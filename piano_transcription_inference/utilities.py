3640acc6c26c649c63ad079963fbb8ba1f0854c8
Yuanpei Ma <yuanpeima@Yuanpeis-Mac-mini.local>
2024-09-15 10:45:07 -0400

    fix librosa util

--- piano_transcription_inference/utilities.py
+++ piano_transcription_inference/utilities.py
@@ -520,7 +520,7 @@ def load_audio(path, sr=22050, mono=True, offset=0.0, duration=None,
         n = 0
 
         for frame in input_file:
-            frame = librosa.core.audio.util.buf_to_float(frame, dtype=dtype)
+            frame = librosa.util.buf_to_float(frame, dtype=dtype)
             n_prev = n
             n = n + len(frame)
 
